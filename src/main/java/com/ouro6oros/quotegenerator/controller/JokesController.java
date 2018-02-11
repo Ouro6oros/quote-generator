5cc93bf110668fb94beb9034164b094d629b2d30
Kenneth Yip <kyipnz@gmail.com>
2018-02-11 21:34:32 +1300

    Added View

--- src/main/java/com/ouro6oros/quotegenerator/controller/JokesController.java
+++ src/main/java/com/ouro6oros/quotegenerator/controller/JokesController.java
@@ -3,13 +3,26 @@ package com.ouro6oros.quotegenerator.controller;
 import com.ouro6oros.quotegenerator.service.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
-    QuoteService quoteService;
+    private QuoteService quoteService;
 
-    public JokesController(@Autowired QuoteService quoteService) {
+    @Autowired
+    public JokesController(QuoteService quoteService) {
         this.quoteService = quoteService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getQuote(Model model){
+
+        model.addAttribute("quote", quoteService.getQuote());
+
+        return "chucknorris";
+
+
     }
 }
